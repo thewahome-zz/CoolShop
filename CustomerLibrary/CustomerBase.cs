29fe86f7041145cd5b91755e9cab60f20040e7cb
Charles Wahome <cwahome@virtualcity.co.ke>
2016-03-30 12:34:02 +0300

    Coolshop with User interface

---
+++ CustomerLibrary/CustomerBase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerLibrary
+{
+    public class CustomerBase
+    {
+        public string CustomerName { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal BillAmount { get; set; }
+        public DateTime BillDate { get; set; }
+        public string Address { get; set; }
+        public virtual void Validate() { }
+    }
+
+    public class Customer: CustomerBase
+    {
+        public override void Validate()
+        {
+            if (CustomerName.Length == 0)
+            {
+                throw new Exception("Customer name is required");
+            }
+            if (PhoneNumber.Length == 0)
+            {
+                throw new Exception("Phone number is required");
+            }
+            if (BillAmount > 0)
+            {
+                throw new Exception("Bill amount is required");
+            }
+            if (BillDate>=DateTime.Now)
+            {
+                throw new Exception("Bill date is not proper");
+            }
+
+        }
+
+    }
+    public class Lead: CustomerBase
+    {
+        public override void Validate()
+        {
+            if (CustomerName.Length == 0)
+            {
+                throw new Exception("Customer name is required");
+            }
+            if (PhoneNumber.Length == 0)
+            {
+                throw new Exception("Phone number is required");
+            }
+        }
+ 
+    }
+}
---
+++ CustomerUI/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CustomerLibrary;
+
+namespace CustomerUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            CustomerBase customerBase = null;
+            if (CboCustomerType.SelectedIndex == 0)
+            {
+                customerBase = new Lead();
+            }
+            else
+            {
+                customerBase= new Customer();
+            }
+            customerBase.CustomerName = TxtCustomerName.Text;
+            customerBase.Address = TextBoxAddress.Text;
+            customerBase.PhoneNumber = TextBoxPhoneNumber.Text;
+            customerBase.BillDate = Convert.ToDateTime(TxtBillDate.Text);
+            customerBase.BillAmount = Convert.ToDecimal(TextBoxBillAmount.Text);
+        }
+    }
+}
