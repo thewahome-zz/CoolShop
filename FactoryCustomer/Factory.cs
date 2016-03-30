b6044913904ab73c57c8f7bbbb625c3be28e3d7e
Charles Wahome <cwahome@virtualcity.co.ke>
2016-03-30 16:21:30 +0300

    automatic lazy loading

---
+++ FactoryCustomer/Factory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using CustomerLibrary;
+using ICustomerInterface;
+
+namespace FactoryCustomer
+{
+    public static class Factory
+    {
+        private static Lazy<List<ICustomer>> CustomersList = null;
+
+        static Factory()
+        {
+            CustomersList = new Lazy<List<ICustomer>>(() => LoadCustomers());
+        }
+
+        private static List<ICustomer> LoadCustomers() 
+        {
+            List<ICustomer> listOfCustomers= new List<ICustomer>();
+            listOfCustomers.Add(new Lead());
+            listOfCustomers.Add(new Customer());
+            return listOfCustomers;
+        }
+        public static ICustomer CreateCustomer(CustomerType customerType)
+        {
+            return CustomersList.Value[(int)customerType];
+        }
+    }
+}
